5bbcf1f2d16b1678a6a1e94d737f47722b74f5e1
paracoders <shivamupadhyaycsbe@gmail.com>
2024-02-21 21:36:59 +0530

    Alpha blog crud operations with routs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,54 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def show
-        #binding.break #Insted of byebug this is new way to debug rails 7 application 
-        @article = Article.find(params[:id])
+        #binding.break is used to debug insted of byebug
     end
 
     def index
         @articals = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to article_path(@article) #or we can directly use redirect_to @article
+        else 
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
